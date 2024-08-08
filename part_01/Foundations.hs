c5fcca80c22d100af5b202193ed5dc39e91c9375
Aze <syedazeez337@gmail.com>
2024-08-08 10:48:57 -0400

    Added functions

--- part_01/Foundations.hs
+++ part_01/Foundations.hs
@@ -17,3 +17,22 @@ mysum (x:xs) = x + mysum xs
 sumGauss :: Integral a => a -> a
 sumGauss n = (n * (n + 1) `div`) 2
 
+-- Standard prelude functions
+myhead :: [a] -> a
+myhead (x:xs) = x
+
+mytail :: [a] -> [a]
+mytail (x:xs) = xs
+
+-- take function
+mytake :: (Eq t, Num t) => t -> [a] -> [a]
+mytake 0 [] = []
+mytake 0 (x:xs) = []
+mytake n (x:xs) = x : mytake (n-1) xs
+
+-- URL Builder
+urlBuilder com search = "https://" ++
+                        com ++
+                        ".com" ++
+                        "/" ++
+                        search
