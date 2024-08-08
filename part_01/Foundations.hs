60e4e37deab54fec4478471cd7762caf7d3a8ac2
Aze <syedazeez337@gmail.com>
2024-08-08 08:23:40 -0400

    Added Functions

--- part_01/Foundations.hs
+++ part_01/Foundations.hs
@@ -7,3 +7,13 @@ simple x = x
 myrev :: [a] -> [a]
 myrev [] = []
 myrev (x:xs) = myrev xs ++ [x]
+
+-- Sum of a finite list of elements
+mysum :: Num t => [t] -> t
+mysum [] = 0
+mysum (x:xs) = x + mysum xs
+
+-- Sum of numbers from 1 to n
+sumGauss :: Integral a => a -> a
+sumGauss n = (n * (n + 1) `div`) 2
+
